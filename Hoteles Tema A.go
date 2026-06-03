74423506c1866d41d03b9658be9a4a89fbeeab3a
Alancito Joel <alanjoelgaleano500@gmail.com>
2026-06-02 22:59:27 -0300

    Añadido

--- Hoteles Tema A.go
+++ Hoteles Tema A.go
@@ -4,15 +4,21 @@ AMBIENTE
 	reservas: secuencia de entero
 	salida: secuencia de entero
 	hot: caracter
-	resv, i, j, cant_noches, mayor_resv, n_resv: entero
+	resv, i, j, cant_noches, mayor_resv, n_resv, cant_resv: entero
 	procedimiento salida() ES
 		si resv < 90000 entonces
 			ESC(salida, n_resv);
 			ESC(salida, resv);
 		fsi;
 	fprocedimiento.
+	procedimiento digitosresv() ES
+		para i:= 1 hasta 3 hacer
+			cant_resv:= cant_resv*10 + ConvertiraNumero(hot);
+			AVZ(hotel, hot);
+		fpara;
+	fprocedimiento.
 	procedimiento reserva() ES
-		Para i:= 1 hasta ConvertiraNumero(hot) hacer
+		Para i:= 1 hasta cant_resv hacer
 			n_resv:= resv;
 			Para j:= 1 hasta 2 hacer
 				AVZ(reservas, resv);
@@ -34,6 +40,7 @@ PROCESO
 			AVZ(hotel, hot);
 		fmientras;
 		AVZ(hotel, hot);
+		digitosresv()
 		cant_noches:= 0;
 		reserva();
 		Escribir("La reserva con mayor cantidad de noches fue la: ", mayor_resv, "Y la cantidad de noches fue de: ", cant_noches);
--- Supermercado Tema A.go
+++ Supermercado Tema A.go
@@ -4,15 +4,20 @@ AMBIENTE
 	ventas: secuencia de entero
 	salida: secuencia de entero
 	suc: caracter
-	vent, i, j, mayor_imp, mayor_tick, n_tick: entero
+	vent, i, j, mayor_imp, mayor_tick, n_tick, cant_tick: entero
 	procedimiento salida() ES
 		si vent > 50000 entonces
 			ESC(salida, n_tick);
 			ESC(salida, vent);
 		fsi;
 	fprocedimiento.
+	procedimiento digitostick() ES
+		para i:= 1 hasta 3 hacer
+			cant_tick:= cant_tick*10 + ConvertiraNumero(suc);
+			AVZ(sucursales, suc);
+		fpara;
 	procedimiento importe() ES
-		Para i:= 1 hasta ConvertiraNumero(suc) hacer
+		Para i:= 1 hasta cant_tick hacer
 			AVZ(ventas, vent);
 			n_tick:= vent;
 			Para j:= 1 hasta 2 hacer
@@ -35,6 +40,7 @@ PROCESO
 			AVZ(sucursales, suc);
 		fmientras;
 		AVZ(sucursales, suc);
+		digitostick();
 		mayor_imp:= 0;
 		importe();
 		Escribir("El ticket de mayor importe fue el: ", mayor_tick, "Y el importe fue de: ", mayor_imp);
